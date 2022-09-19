168c36de9efa388fc7a15f210fecd5957e54f420
agamgunawan <63200342+agamgunawan@users.noreply.github.com>
2022-09-18 20:16:06 -0700

    Update SupermarketSales.py

--- SupermarketSales.py
+++ SupermarketSales.py
@@ -2,8 +2,5 @@ import pandas as pd
 import numpy as np
 import streamlit  as st
 
-st.write("""
-# Supermarket Grocery Sales
-Supermarket sales report analysis from 2014-2018.
-""")
-
+st.title('Supermarket Grocery Sales')
+st.write("Supermarket sales report analysis from 2014-2018.")
