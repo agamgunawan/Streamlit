f0355177141156465013a45914054906defd2c62
agamgunawan <63200342+agamgunawan@users.noreply.github.com>
2022-09-20 00:48:00 -0700

    Update SupermarketSales.py

--- SupermarketSales.py
+++ SupermarketSales.py
@@ -6,3 +6,6 @@ st.write("""
 # Supermarket Grocery Sales
 Supermarket sales report analysis from 2014-2018.""")
 
+df = pd.read_csv("Supermart Grocery Sales - Retail Analytics Dataset.csv")
+
+st.write(df)
